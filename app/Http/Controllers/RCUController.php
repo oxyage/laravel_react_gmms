c71c17c66f4c2ccdee3c78dce4e82c09d33e14ab
oxyage <oxyage@yandex.ru>
2020-04-26 23:48:04 +0300

    Add model (route, controller, migrations) for RCU, RTS

---
+++ app/Http/Controllers/RCUController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\RCU;
+use Illuminate\Http\Request;
+
+class RCUController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\RCU  $rCU
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RCU $rCU)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\RCU  $rCU
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RCU $rCU)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\RCU  $rCU
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RCU $rCU)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\RCU  $rCU
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RCU $rCU)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/RTSController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\RTS;
+use Illuminate\Http\Request;
+
+class RTSController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\RTS  $rTS
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RTS $rTS)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\RTS  $rTS
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RTS $rTS)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\RTS  $rTS
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RTS $rTS)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\RTS  $rTS
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RTS $rTS)
+    {
+        //
+    }
+}
